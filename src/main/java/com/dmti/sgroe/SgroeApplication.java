87724a3eba45552fc8cf7bab51d82d7e4c768b39
David Melo <davidr7melo@hotmail.com>
2019-10-31 23:05:39 -0400

    Primeiro commit - projeto criado

---
+++ src/main/java/com/dmti/sgroe/SgroeApplication.java
@@ -0,0 +1,13 @@
+package com.dmti.sgroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgroeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmti/sgroe/SgroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmti.sgroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

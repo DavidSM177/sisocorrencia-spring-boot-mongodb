4612525e01c73d9cc58ddb02a9683d2bbdc85204
David Melo <davidr7melo@hotmail.com>
2019-11-23 22:34:09 -0400

    Obtendo um usuário por id

--- src/main/java/com/dmti/sgroe/services/UserService.java
+++ src/main/java/com/dmti/sgroe/services/UserService.java
@@ -1,12 +1,14 @@
 package com.dmti.sgroe.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dmti.sgroe.domain.User;
 import com.dmti.sgroe.repository.UserRepository;
+import com.dmti.sgroe.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 
 }
